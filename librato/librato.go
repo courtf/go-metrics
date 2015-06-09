95cf3775056e20f17ed378839cc9c34b670ed422
Court Fowler <courtf@gmail.com>
2015-06-09 11:14:29 -0700

    merge with yvasiyarov/go-metrics

--- librato/librato.go
+++ librato/librato.go
@@ -7,7 +7,7 @@ import (
 	"regexp"
 	"time"
 
-	"github.com/rcrowley/go-metrics"
+	"github.com/courtf/go-metrics"
 )
 
 // a regexp for extracting the unit from time.Duration.String
@@ -28,11 +28,20 @@ type Reporter struct {
 	Registry        metrics.Registry
 	Percentiles     []float64              // percentiles to report on histogram metrics
 	TimerAttributes map[string]interface{} // units in which timers will be displayed
-	intervalSec     int64
+	MetricPrefix    string
+	QuietMode       bool
 }
 
 func NewReporter(r metrics.Registry, d time.Duration, e string, t string, s string, p []float64, u time.Duration) *Reporter {
-	return &Reporter{e, t, s, d, r, p, translateTimerAttributes(u), int64(d / time.Second)}
+	return &Reporter{
+		Email:           e,
+		Token:           t,
+		Source:          s,
+		Interval:        d,
+		Registry:        r,
+		Percentiles:     p,
+		TimerAttributes: translateTimerAttributes(u),
+	}
 }
 
 func Librato(r metrics.Registry, d time.Duration, e string, t string, s string, p []float64, u time.Duration) {
@@ -43,15 +52,19 @@ func (self *Reporter) Run() {
 	ticker := time.Tick(self.Interval)
 	metricsApi := &LibratoClient{self.Email, self.Token}
 	for now := range ticker {
+
 		var metrics Batch
 		var err error
 		if metrics, err = self.BuildRequest(now, self.Registry); err != nil {
-			log.Printf("ERROR constructing librato request body %s", err)
-			continue
+			if self.QuietMode == false {
+				log.Printf("ERROR constructing librato request body %s", err)
+			}
 		}
+
 		if err := metricsApi.PostMetrics(metrics); err != nil {
-			log.Printf("ERROR sending metrics to librato %s", err)
-			continue
+			if self.QuietMode == false {
+				log.Printf("ERROR sending metrics to librato %s", err)
+			}
 		}
 	}
 }
@@ -79,16 +92,21 @@ func sumSquaresTimer(t metrics.Timer) float64 {
 
 func (self *Reporter) BuildRequest(now time.Time, r metrics.Registry) (snapshot Batch, err error) {
 	snapshot = Batch{
-		// coerce timestamps to a stepping fn so that they line up in Librato graphs
-		MeasureTime: (now.Unix() / self.intervalSec) * self.intervalSec,
+		MeasureTime: now.Unix(),
 		Source:      self.Source,
 	}
+	snapshot.MeasureTime = now.Unix()
 	snapshot.Gauges = make([]Measurement, 0)
 	snapshot.Counters = make([]Measurement, 0)
 	histogramGaugeCount := 1 + len(self.Percentiles)
 	r.Each(func(name string, metric interface{}) {
+
+		if self.MetricPrefix != "" {
+			name = self.MetricPrefix + name
+		}
 		measurement := Measurement{}
 		measurement[Period] = self.Interval.Seconds()
+
 		switch m := metric.(type) {
 		case metrics.Counter:
 			if m.Count() > 0 {
@@ -115,9 +133,9 @@ func (self *Reporter) BuildRequest(now time.Time, r metrics.Registry) (snapshot 
 				s := m.Sample()
 				measurement[Name] = fmt.Sprintf("%s.%s", name, "hist")
 				measurement[Count] = uint64(s.Count())
+				measurement[Sum] = s.Sum()
 				measurement[Max] = float64(s.Max())
 				measurement[Min] = float64(s.Min())
-				measurement[Sum] = float64(s.Sum())
 				measurement[SumSquares] = sumSquares(s)
 				gauges[0] = measurement
 				for i, p := range self.Percentiles {
